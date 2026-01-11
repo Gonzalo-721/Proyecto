15f62302f22bdf607e8f61361be36fe08d48bf5b
Gonzalo-721 <dgal75736@gmail.com>
2026-01-11 07:56:52 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from sqlalchemy.orm import joinedload
 from models import db, Usuario, Cliente, Empleado, Habitacion, Reserva, Servicio, ConsumoServicio, Pago
 from datetime import datetime
@@ -473,6 +473,7 @@ def logout():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
