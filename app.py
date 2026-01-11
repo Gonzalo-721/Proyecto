1747029d998a0553507be9c4ff043c9c934f961c
Gonzalo-721 <dgal75736@gmail.com>
2026-01-11 10:54:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -386,18 +386,10 @@ def editar_servicios(id_reserva):
 
     if request.method == 'POST':
         accion = request.form.get('accion')
-
-        if accion == "cancelar":
-            nuevos_consumos = ConsumoServicio.query.filter(
-                ConsumoServicio.id_reserva == id_reserva,
-                ~ConsumoServicio.id_consumo.in_(original_ids)
-            ).all()
-            for c in nuevos_consumos:
-                db.session.delete(c)
-            db.session.commit()
-
-            flash("Cambios cancelados", "info")
-            return redirect(url_for('detalles_reserva', id_reserva=id_reserva))
+        
+    if accion == "cancelar":
+        flash("Cambios cancelados", "warning")
+        return redirect(url_for('detalles_reserva', id_reserva=id_reserva))
 
         elif accion == "añadir":
             id_servicio_nuevo = request.form.get('id_servicio_nuevo')
@@ -553,6 +545,7 @@ def logout():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
